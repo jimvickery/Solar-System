371134b299d6fe2d18588dabb3bd8edc60306442
Jim Vickery <jimrv8@yahoo.com>
2017-08-11 13:39:42 -0500

    updated main.js and index.html

--- array-methods.js
+++ array-methods.js
@@ -1,29 +1,62 @@
 var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
-
+// ======================================================================================
 /*
  Use the forEach method to add the name of each planet
  to a div element in your HTML with an id of "planets".
 */
-var planetsEl = document.getElementById("planets");
-// Use the map method to create a new array where the first letter of each planet is capitalized
-
-let capPlanets = "";
-
+let displayPlanets = "";
 planets.forEach(function(item){
-//    capPlanets += `${planets.item}.map(${planets.item}.toUpperCase());` 
-    capPlanets += `${item}.map(${item.toUpper};` 
+    displayPlanets += `${item} `;
 });
- 
 
+//get placehodller for planets
+var planetsEl = document.getElementById("planets");
 
- planetsEl.innerHTML = capPlanets;
+//set planet elements = to capPlanets
+planetsEl.innerHTML = displayPlanets;
 
-// Use the filter method to create a new array that contains planets with the letter 'e'
+// ======================================================================================
+// Use the map method to create a new array where the first letter of each planet is capitalized
+let capEl = document.getElementById("cap-planets");
+function capitalize(string){
+    return string[0].toUpperCase() + string.slice(1);
+   }
 
+ capPlanets = planets.map(function(item){
+    return capitalize(item);
+     
+});
+// console.log('cap item', capPlanets);
+capEl.innerHTML = capPlanets;
+// ======================================================================================
+
+// Use the filter method to create a new array that contains planets with 
+// the letter 'e'
+//placeholder for planets with e
+let ePlanetsEl = document.getElementById("e-planets");
+// let ePlanets = planets.filter(planetsWithE)
+
+var ePlanetNames = planets.filter(function(word){
+    return /e/.test(word);
+})
+ePlanetsEl.innerHTML = ePlanetNames;
+
+// ======================================================================================
 
 // Use the reduce method to create a sentence from the words in the following array
 
 var wordsEl = document.getElementById('sentence');
-
 var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
+
+function arrConvertedToSentence(array){
+    var  lastOne = array.pop();
+    return array.join(' ')  + lastOne;
+}
+let arrSentence = arrConvertedToSentence(words);
+
+// console.log('arrySentence', arrSentence);
+wordsEl.innerHTML = arrSentence;
+
+// ======================================================================================
+
 
